22e0e7d9050ee29e77c62f9c953aef23ce795ba3
sawand98d <sawant65d@gmail.com>
2021-05-29 13:40:05 +0530

    resolved error updated list name to STATIC_DIR to STATICFILES_DIR

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,11 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'staticfiles')
 ]
