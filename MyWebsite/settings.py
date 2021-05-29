7d8e8cae0069aef4c940bf7758294c72bb42f0d9
sawand98d <sawant65d@gmail.com>
2021-05-29 13:12:38 +0530

    Added static files, css and html files

--- MyWebsite/settings.py
+++ MyWebsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -117,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_DIRS = [
+    os.path.join(BASE_DIR,'staticfiles')
+]
--- MyWebsite/urls.py
+++ MyWebsite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from base import urls as base_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(base_urls))
 ]
